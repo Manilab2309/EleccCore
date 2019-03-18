82983555b0afd69f8b539ca9d2422d9022e6c19a
Ramón Cigüenza <rcifuster@gmail>
2019-03-18 13:31:59 +0100

    Fix test to launch

--- src/test/java/com/elecc/tests/application/EleccCoreApplicationTests.java
+++ src/test/java/com/elecc/tests/application/EleccCoreApplicationTests.java
@@ -1,16 +1,19 @@
 package com.elecc.tests.application;
 
+import com.elecc.application.EleccCoreApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={EleccCoreApplication.class})
 public class EleccCoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+
 	}
 
 }
